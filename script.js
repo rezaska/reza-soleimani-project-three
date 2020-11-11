9d42b62e7866c337fb81e0d01467f94b5b9e888f
rezaska <reza.soleimani.ka@gmail.com>
2020-11-11 00:07:05 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Joker! Run!! game.
+
+// check whether the document is ready for JS.
+
+// have an event listener for click on one of the cards.
+
+// prevent the default behavior.
+
+// create a loop through the chosen card:
+
+// if the first selected card is joker then the game is over (show "game over" message).
+
+// if the selected card is not joker then let the user choose second card (show "choose the next card" message).
+
+// if the second selected card is joker then the game is over (show "game over" message).
+
+// if the second selected card is not joker then the user win (show "you win" message).
+
+//--------------------------------------------------------
+
